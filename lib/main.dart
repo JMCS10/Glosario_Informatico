da068b3c0108b7fafccf9d2f8b0806a89da2ce2e
Fabio-3 <202200065@est.umss.edu>
2025-09-25 15:51:09 -0400

    mensaje
    agrege un mensaje en main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';//sssssss
 import 'conexion.dart'; //para importar la conexion
 import 'pantallas/pantalla_inicio.dart'; // Importamos la pantalla de inicio
 
